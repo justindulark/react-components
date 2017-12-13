c912e016c0bb10332241c164f83691c05b22bc40
justin <justindulark@gmail.com>
2017-12-13 14:01:09 -0800

    Implement react solution with es6 and jsx

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 // TODO
+var GroceryList = (props) => (
+  <div>
+    {props.items.map(item =>
+      <GroceryListItem name={item} />
+    )}
+  </div>
+);
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hover: false
+    }
+  }
+  onItemHover() {
+    this.setState({
+      hover: !this.state.hover
+    })
+  }
+  render() {
+    var style = {
+      fontWeight: this.state.hover ? 'bold' : 'normal'
+    }
+    return(
+      <li style={style} onMouseEnter={this.onItemHover.bind(this)} onMouseLeave={this.onItemHover.bind(this)}>{this.props.name}</li>
+    );
+  }
+}
+
+ReactDOM.render(
+  <GroceryList items={['Eggs', 'Milk']}/>, 
+  document.getElementById('app')
+);
